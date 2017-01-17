884516879ae0c093933de70fae5a8b58f3891548
Ilya Skorik <ilya@skorik.co>
2017-01-18 00:19:46 +0300

    Add grunt-concurrent support

--- gruntfile.js
+++ gruntfile.js
@@ -16,17 +16,33 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      styles: {
-        files: ['**/*.scss'],
-        tasks: ['sass'],
+      watch_1: {
+        files: ['style1.scss'],
+        tasks: ['sass:entry_point1'],
         options: {
           spawn: false
         }
+      },
+      watch_2: {
+        files: ['style2.scss'],
+        tasks: ['sass:entry_point2'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+    concurrent: {
+      options: {
+        logConcurrentOutput: true
+      },
+      watch: {
+        tasks: ["watch:watch_1", "watch:watch_2"]
       }
     }
   });
   
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['sass', 'watch']);
+  grunt.registerTask('default', ['sass', 'concurrent:watch']);
 };
